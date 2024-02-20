4f6563553b6143bce19ca64a56dfdd3b144b48c0
kistgab <gabrielkist02@gmail.com>
2024-02-20 20:43:07 -0300

    test: ensure pro will be paired with CalculateDarkMatterNasa if has the necessary score

--- src/usecases/pro/pair-with-project/pair-with-project.pro.usecase.spec.ts
+++ src/usecases/pro/pair-with-project/pair-with-project.pro.usecase.spec.ts
@@ -1,37 +1,62 @@
 import { ProEntity } from '@/domain/entities/pro/pro.entity';
 import { EducationLevel } from '@/domain/enums/education-levels.enum';
-import { PairProWithProjectInput } from '@/usecases/pro/pair-with-project/pair-with-project.pro.dto';
+import { Project } from '@/domain/enums/projects.enum';
+import {
+  PairProWithProjectInput,
+  PairProWithProjectOutput,
+} from '@/usecases/pro/pair-with-project/pair-with-project.pro.dto';
 import { PairProWithProjectUseCase } from '@/usecases/pro/pair-with-project/pair-with-project.pro.usecase';
 
 function createSut(): PairProWithProjectUseCase {
   return new PairProWithProjectUseCase();
 }
 
-function createFakeInput(): PairProWithProjectInput {
+function createFakeInput(
+  customProps?: Partial<PairProWithProjectInput>,
+): PairProWithProjectInput {
   return {
-    age: 35,
-    educationLevel: EducationLevel.NoEducation,
-    pastExperiences: {
-      sales: false,
-      support: false,
+    age: customProps?.age || 25,
+    educationLevel:
+      customProps?.educationLevel || EducationLevel.BachelorsDegreeOrHigher,
+    pastExperiences: customProps?.pastExperiences || {
+      sales: true,
+      support: true,
     },
-    internetTest: {
-      downloadSpeed: 40.2,
-      uploadSpeed: 40.2,
+    internetTest: customProps?.internetTest || {
+      downloadSpeed: 100,
+      uploadSpeed: 100,
     },
-    writingScore: 0.6,
-    referralCode: 'any_referral_code',
+    writingScore: customProps?.writingScore || 1,
+    referralCode: customProps?.referralCode || 'token1234',
   };
 }
 
 describe('PairProWithProject - Use Case', () => {
   it('should call the ProEntity to calculate the Pro score', () => {
     const calculateScoreSpy = jest.spyOn(ProEntity.prototype, 'calculateScore');
-    const input = createFakeInput();
     const sut = createSut();
 
-    sut.pair(input);
+    sut.pair(createFakeInput());
 
     expect(calculateScoreSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it("should pair the Pro to the project 'calculate_dark_matter_nasa' when have the necessary score", () => {
+    const sut = createSut();
+
+    const result = sut.pair(createFakeInput());
+
+    const expectedResult: PairProWithProjectOutput = {
+      score: 15,
+      selectedProject: Project.CalculateDarkMatterNasa,
+      eligibleProjects: [
+        Project.CalculateDarkMatterNasa,
+        Project.CollectInformationForXpto,
+        Project.DetermineSchrodingerCatIsAlive,
+        Project.SupportUsersFromXyz,
+      ],
+      ineligibleProjects: [],
+    };
+    expect(result).toEqual(expectedResult);
   });
 });
