48585cbd0aadf64d4923ad64ca9dc38822ae54aa
kistgab <gabrielkist02@gmail.com>
2024-02-20 10:24:30 -0300

    chore: configure jest with path mappings

---
+++ jest.config.ts
@@ -0,0 +1,15 @@
+import { pathsToModuleNameMapper } from 'ts-jest';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { compilerOptions } = require('./tsconfig.json');
+
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  modulePaths: [compilerOptions.baseUrl],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths),
+  coverageDirectory: '../coverage',
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  testRegex: '.*\\.spec\\.ts$',
+  transform: { '^.+\\.(t|j)s$': 'ts-jest' },
+  collectCoverageFrom: ['**/*.(t|j)s'],
+};
