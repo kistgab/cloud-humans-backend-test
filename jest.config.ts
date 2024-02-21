b37da03f55eec674efb224084d50c6b82d5e9b6e
kistgab <gabrielkist02@gmail.com>
2024-02-21 11:45:36 -0300

    chore: jest config for integration and e2e tests

--- jest.config.ts
+++ jest.config.ts
@@ -1,16 +1,21 @@
+import type { Config } from 'jest';
 import { pathsToModuleNameMapper } from 'ts-jest';
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { compilerOptions } = require('./tsconfig.json');
 
-export default {
+const config: Config = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   modulePaths: [compilerOptions.baseUrl],
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths),
   coverageDirectory: 'coverage',
   moduleFileExtensions: ['js', 'json', 'ts'],
-  testRegex: '.*\\.spec\\.ts$',
-  transform: { '^.+\\.(t|j)s$': 'ts-jest' },
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  testMatch: ['**/*.spec.ts', '**/*.integration-spec.ts', '**/*.e2e-spec.ts'],
   collectCoverageFrom: ['<rootDir>/src/**/*.(t|j)s'],
   modulePathIgnorePatterns: ['<rootDir>/test/'],
 };
+
+export default config;
