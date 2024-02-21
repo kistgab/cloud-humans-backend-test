9f7fda03051c09b81a7c655b050f57da0a3c85bf
kistgab <gabrielkist02@gmail.com>
2024-02-21 10:12:52 -0300

    fix: delete unused file

--- src/domain/enums/projects.enum.ts
+++
@@ -1,6 +0,0 @@
-export enum Project {
-  CalculateDarkMatterNasa = 'calculate_dark_matter_nasa',
-  DetermineSchrodingerCatIsAlive = 'determine_schrodinger_cat_is_alive',
-  SupportUsersFromXyz = 'support_users_from_xyz',
-  CollectInformationForXpto = 'collect_information_for_xpto',
-}
