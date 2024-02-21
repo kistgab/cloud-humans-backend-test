cb3410b7e3bfd15dafa8e1ce9f68e901055bf0cb
kistgab <gabrielkist02@gmail.com>
2024-02-21 19:08:53 -0300

    feat: ensure FakeDatabaseProject loads the json correctly

--- src/infrastructure/project/repository/fake-database/fake-database-project.repository.ts
+++ src/infrastructure/project/repository/fake-database/fake-database-project.repository.ts
@@ -1,11 +1,12 @@
 import { ProjectEntity } from '@/domain/project/entity/project.entity';
 import { FindEligibleProjectsRepository } from '@/domain/project/repository/find-eligible-projects.repository';
+import { FindIneligibleProjectsRepository } from '@/domain/project/repository/find-ineligible-projects-repository';
 import { ProjectMapper } from '@/infrastructure/project/mapper/project.mapper';
 import { ProjectModel } from '@/infrastructure/project/repository/fake-database/project.model';
 import * as fs from 'fs';
 
 export class FakeDatabaseProjectRepository
-  implements FindEligibleProjectsRepository
+  implements FindEligibleProjectsRepository, FindIneligibleProjectsRepository
 {
   async findAllEligibles(score: number): Promise<ProjectEntity[]> {
     const projectsData = this.loadProjects();
@@ -13,6 +14,12 @@ export class FakeDatabaseProjectRepository
       .filter((project) => project.minimum_score < score)
       .map(ProjectMapper.toEntity);
     return eligibleProjects;
+  }
+
+  async findAllIneligibles(proScore: number): Promise<ProjectEntity[]> {
+    proScore;
+    this.loadProjects();
+    return [];
   }
 
   private loadProjects(): ProjectModel[] {
