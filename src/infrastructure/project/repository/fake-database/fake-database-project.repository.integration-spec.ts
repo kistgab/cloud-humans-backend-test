00d75ecd00d51955ead923a15397d93b4f3485c5
kistgab <gabrielkist02@gmail.com>
2024-02-21 13:12:40 -0300

    test: ensure FakeDatabaseProjectRepository returns only projects that Pro has enough score

--- src/infrastructure/project/repository/fake-database/fake-database-project.repository.integration-spec.ts
+++ src/infrastructure/project/repository/fake-database/fake-database-project.repository.integration-spec.ts
@@ -1,3 +1,4 @@
+import { ProjectEntity } from '@/domain/project/entity/project.entity';
 import { ProjectMapper } from '@/infrastructure/project/mapper/project.mapper';
 import { FakeDatabaseProjectRepository } from '@/infrastructure/project/repository/fake-database/fake-database-project.repository';
 import { readFileSync } from 'fs';
@@ -17,6 +18,19 @@ describe('FakeDatabaseProject - Repository', () => {
       const projects = await sut.findAllEligibles(100);
 
       expect(projects).toEqual(allProjects);
+    });
+
+    it('should only the projects that Pro can be eligible by his score', async () => {
+      const sut = createSut();
+      const allEligibleProjects = JSON.parse(
+        readFileSync('data/static-projects.json', 'utf-8'),
+      )
+        .projects.map(ProjectMapper.toEntity)
+        .filter((p: ProjectEntity) => p.minimumScore < 6);
+
+      const projects = await sut.findAllEligibles(6);
+
+      expect(projects).toEqual(allEligibleProjects);
     });
   });
 });
