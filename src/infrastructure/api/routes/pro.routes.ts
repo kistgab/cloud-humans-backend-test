75a5635cc8d2f5f72117070cfa9d642f1c6695cd
kistgab <gabrielkist02@gmail.com>
2024-02-21 21:54:33 -0300

    feat: ensure that PairWithProject route returns 200 on success

--- src/infrastructure/api/routes/pro.routes.ts
+++ src/infrastructure/api/routes/pro.routes.ts
@@ -1,10 +1,11 @@
 import { RequestPairWithProject } from '@/infrastructure/pro/dto/pair/request-pair-with-project.dto';
 import { PairProWithProjectController } from '@/presentation/pro/controllers/pair-with-project.pro.controller';
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 
 @Controller('pros')
 export class ProRoutes {
   @Post('pairWithProject')
+  @HttpCode(HttpStatus.OK)
   async pairProWihProject(@Body() request: RequestPairWithProject) {
     const controller = new PairProWithProjectController();
     const response = controller.handle(request);
